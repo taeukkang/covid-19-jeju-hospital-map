850734a7d35ecc5c1c0beaea2333574fd609528a
taeukme <taeukme@users.noreply.github.com>
2020-07-19 02:56:10 +0000

    deploy: 4de1edfce306f3737e9ec148eef84dd49a79a3d7

---
+++ precache-manifest.fca9da278766ecfca8fa6aa187b08a76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "493bb4db1e8720e3bf160e644aac390a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d0477b5be2bd254b8a3",
+    "url": "/static/css/main.15d31c9d.chunk.css"
+  },
+  {
+    "revision": "c27ea31222c14a5dc153",
+    "url": "/static/js/2.bcdeb281.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.bcdeb281.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d0477b5be2bd254b8a3",
+    "url": "/static/js/main.2c627e9d.chunk.js"
+  },
+  {
+    "revision": "8e65dc9c065515220c5a",
+    "url": "/static/js/runtime-main.a99765c6.js"
+  }
+]);
